fa50f35eb52a8acc2b6f037f5ad1670bbf8c8c75
adeearyaa <adeearyaa2013@gmail.com>
2024-06-11 10:03:53 +0800

    Update deprecated modules

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 
-merger = PyPDF2.PdfFileMerger()
+merger = PyPDF2.PdfMerger()
 for file in os.listdir(os.curdir):
     if file.endswith(".pdf"):
         merger.append(file)
